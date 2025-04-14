3b7992cb05d8bdff9aa52d409e65e54393a37fa1
Solentra <soulxz2007@gmail.com>
2025-04-14 02:02:53 -0500

    Update Docs.lua

--- Docs.lua
+++ Docs.lua
@@ -1,2 +1,1 @@
-```lua
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+<pre> ```lua local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))() ``` </pre>
